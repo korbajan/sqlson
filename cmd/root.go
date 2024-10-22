6a51b7550015be2fd8a103c626b8d6471cec76f5
Jakub Stepien <jakub.kuba.stepien@gmail.com>
2024-10-22 23:18:58 +0200

    refactor cmd/root.go

--- cmd/root.go
+++ cmd/root.go
@@ -9,16 +9,15 @@ import (
   "github.com/korbajan/sqlson/pkg/databases"
 )
 
-func parseFromArgs() (*configs.Database, string) {
-  databaseConfig := configs.NewDatabaseConfig()
-  var query string
-  flags.Parse(&databaseConfig.Host, &databaseConfig.Port, &databaseConfig.Name, &databaseConfig.User, &databaseConfig.Password, &query)
-  return &databaseConfig, query
+func parseFromArgs(databaseConfig *configs.Database, query *string) () {
+  flags.Parse(&databaseConfig.Host, &databaseConfig.Port, &databaseConfig.Name, &databaseConfig.User, &databaseConfig.Password, query)
 }
 
 func Execute() {
-  databaseConfig, sqlQuery := parseFromArgs()
-  jsonString, err := databases.Execute(databaseConfig, sqlQuery)
+  databaseConfig := configs.NewDatabaseConfig()
+  var sqlQuery string
+  parseFromArgs(&databaseConfig, &sqlQuery)
+  jsonString, err := databases.Execute(&databaseConfig, sqlQuery)
   if err != nil {
     fmt.Println(err)
     os.Exit(1)
